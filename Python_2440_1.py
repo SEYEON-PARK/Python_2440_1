876e59d4141e012c0d27a17a26302f1d81173d82
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-08-21 01:43:56 +0900

    Python언어 BAEKJOON 2440번 문제 풀기!(1)

--- Python_2440_1.py
+++ Python_2440_1.py
@@ -9,8 +9,8 @@
 N=int(input()) # 사용자로부터 정수 입력받기
 n=N # n에 N 값 저장하기
 
-for i in range(n):
-    for j in range(N, 0, -1):
+for i in range(n): # i에 0부터 n-1까지 1씩 증가한 값을 대입하면서 반복
+    for j in range(N, 0, -1): # j가 N부터 0이 되기 전까지 -1씩 감소한 값을 대입하면서 반복
         print("*", end="")
     print()
     N-=1
